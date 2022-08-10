d9ba79120c6a49cb5d5b7a2478f57da3ae4fa3f3
= <=>
2022-08-10 05:59:34 -0700

    initial commit

--- sklearn_dashboard/hyperparameters/_tuning_hyperparameters.py
+++ sklearn_dashboard/hyperparameters/_tuning_hyperparameters.py
@@ -8,8 +8,8 @@ number_of_components = len(order_of_components)
 
 hyperparameters = { 
     'ols': None,
-    'lasso': dash_component_label(label='alpha', component_type='slider', 
-                                    component_kwargs={'min': -20, 'max': 20, 'step': 5, 'value': 5}),
+    # 'lasso': dash_component_label(label='alpha', component_type='slider', 
+    #                                 component_kwargs={'min': -20, 'max': 20, 'step': 5, 'value': 5}),
     'lassocv': dash_component_label(label='alpha', component_type='range_slider', 
                                     component_kwargs={'min': -20, 'max': 20, 'step': 5, 'value': [-5, 5]})
 }
@@ -19,6 +19,6 @@ style_hide = {'display': 'none'}
 
 show_hide_hyperparameters = {
     'ols': [style_hide]*number_of_components,
-    'lasso': [style_show, style_hide, style_hide, style_show],
+    # 'lasso': [style_show, style_hide, style_hide, style_show],
     'lassocv': [style_show, style_hide, style_hide, style_show], 
 }
